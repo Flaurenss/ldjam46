2c72a955a22b49ede8afc8afe98adda3fae1e675
s-rom <ing.arl.6876@gmail.com>
2020-04-18 19:43:55 +0200

    Added inventory system

---
+++ LDJ46/Assets/Scripts/PickableBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickableBehaviour : MonoBehaviour
+{
+
+    GameObject keyHint;
+    Animator keyAnimator;
+
+    void Awake()
+    {
+        keyHint = transform.Find("HintCanvas").Find("KeyHint").gameObject;
+        keyAnimator = keyHint.GetComponent<Animator>();
+        SetKeyHint(true);
+    }
+
+
+    
+    public void SetKeyHint(bool state)
+    {
+        Debug.Log("Setting key hint: " + state);
+        keyAnimator.SetBool("Hidden", !state);
+       
+    }
+
+}
---
+++ LDJ46/Assets/Scripts/Pocket.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocket : MonoBehaviour
+{
+
+    private GameObject currentObject;
+    public GameObject CurrentObject
+    {
+        get { return currentObject;  }
+        set 
+        {
+            if (Empty)
+            {
+                currentObject = value;
+                currentObject.SetActive(false);
+                //currentObject.transform.parent = this.gameObject.transform;
+            }
+            else
+            {
+                Debug.Log("Pocket is full");
+            }
+        }
+    }
+
+
+    public bool Empty
+    {
+        get { return currentObject == null; }
+    }
+
+    /// <summary>
+    /// Drops the current object
+    /// </summary>
+    public void Drop()
+    {
+        if (Empty) return;
+
+        this.currentObject.transform.position = this.gameObject.transform.position;
+        this.currentObject.SetActive(true);
+        this.currentObject = null;
+    }
+
+    
+}
