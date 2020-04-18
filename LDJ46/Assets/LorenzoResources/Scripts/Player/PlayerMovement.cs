f6b4c3f8e06223323f4f82ddee6d90269744f370
Flaurenss <lorenzonapl@hotmail.com>
2020-04-18 20:24:24 +0200

    Vampire movement wip

--- LDJ46/Assets/LorenzoResources/Scripts/Player/PlayerMovement.cs
+++ LDJ46/Assets/LorenzoResources/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
---
+++ LDJ46/Assets/LorenzoResources/Scripts/Vampire/VampireController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace LorenzoResources.Scripts.Vampire
+{
+    public class VampireController : MonoBehaviour
+    {
+        [SerializeField] private float vampireSpeed;
+        private Vector2 locationPos;
+        private void Update()
+        {
+            
+        }
+
+        private void FixedUpdate()
+        {
+            var distance = locationPos - (Vector2)transform.position;
+            if (distance.magnitude < 1f)
+            {
+                locationPos = GetRandomPosition();
+            }
+            // transform.Translate();
+        }
+
+        private Vector2 GetRandomPosition()
+        {
+            Vector2 rndDirection = Vector2.zero;
+            var x = Random.Range(-1f,1f);
+            var y = Random.Range(-1f,1f);
+            rndDirection = new Vector2
+            (
+                Random.Range(-1f,1f),
+                Random.Range(-1f,1f)
+            );
+            return Vector2.zero;
+        }
+    }
+}
