6ad20cc1996c25c3a181f9dcefcb199e1f477046
Flaurenss <lorenzonapl@hotmail.com>
2020-04-18 17:28:08 +0200

    Add base player movement, collisions and camera tracker

---
+++ LDJ46/Assets/LorenzoResources/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+
+    public void MovePlayer(Vector3 movement)
+    {
+        var moveDir = movement.normalized * (playerSpeed * Time.deltaTime);
+        transform.position += moveDir;
+    }
+}
