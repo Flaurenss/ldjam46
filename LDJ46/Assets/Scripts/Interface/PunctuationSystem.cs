e5c8f64bb3218816af220b3983d2cc712ce131a9
Flaurenss <lorenzonapl@hotmail.com>
2020-04-20 10:57:09 +0200

    Add basic punctuation script

---
+++ LDJ46/Assets/Scripts/Interface/PunctuationSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PunctuationSystem : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI uiPunctuation;
+    private int _punctuation;
+
+    private void Awake()
+    {
+        _punctuation = 0;
+    }
+
+    private void AddPoints()
+    {
+        _punctuation++;
+        uiPunctuation.text = _punctuation.ToString();
+    }
+}
