5341f6facfd96101243822755fbbfeaf3bf183c9
Flaurenss <lorenzonapl@hotmail.com>
2020-04-20 12:28:12 +0200

    Add punctuation system

--- LDJ46/Assets/Scripts/Interface/PunctuationSystem.cs
+++ LDJ46/Assets/Scripts/Interface/PunctuationSystem.cs
@@ -6,17 +6,25 @@ using UnityEngine;
 
 public class PunctuationSystem : MonoBehaviour
 {
+    [SerializeField] private EventsManager eventsManager;
     [SerializeField] private TextMeshProUGUI uiPunctuation;
     private int _punctuation;
 
     private void Awake()
     {
         _punctuation = 0;
+        ChangePunctuation(_punctuation);
+        eventsManager.gameEventCompleted.AddListener(AddPoints);
     }
 
-    private void AddPoints()
+    private void AddPoints(float f = 0f)
     {
         _punctuation++;
-        uiPunctuation.text = _punctuation.ToString();
+        ChangePunctuation(_punctuation);
+    }
+
+    private void ChangePunctuation(int points)
+    {
+        uiPunctuation.text = points.ToString();
     }
 }
